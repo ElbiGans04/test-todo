00a48d657a0bbf58df75c43706029076f1306f3d
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2022-05-27 17:10:43 +0700

    feat: memperbaharui layout pada src/_app.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,26 @@ import Head from 'next/head'
 import Image from 'next/image'
 import {useEffect} from 'react'
 export default function Home() {
+  useEffect(() => {
+    async function getData () {
+      const body = new URLSearchParams({
+        "name": "elbi",
+        "email": "elbi@data.my.id",
+        "password": "12345678",
+        "password_confirmation": "12345678"
+      })
+      const request = await fetch('https://todos.data.my.id/api/register', {method: 'POST', body: body})
+      console.log(`\n`),
+      console.log(request)
+      return await (request.json())
+    }
+
+    getData().then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
+  })
   return (
     <div>
       <h1 className='text-white underline bg-black'>Hello World</h1>
