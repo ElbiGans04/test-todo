1abbe9738dbf5bc8cb2e7d3608fc421ae6ae4392
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2022-05-28 17:01:48 +0700

    refactor: add Link component ke /register di login
    menambahkan Link component dengan href attribute berupa /register
    di login

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,8 @@
 import Head from 'next/head';
+import Link from 'next/link';
 export default function login() {
   return (
-    <div className="grid items-center w-full grid-rows-3 md:h-64 h-52">
+    <div className="grid items-center w-full grid-rows-3 md:h-80 h-72">
       <Head>
         <title>Login</title>
       </Head>
@@ -34,10 +35,20 @@ export default function login() {
         />
       </div>
 
-      <div className="grid grid-cols-1">
+      <div className="grid items-center grid-cols-1 grid-rows-2 gap-1">
         <button className="px-3 py-1 font-bold rounded-md shadow-md md:px-5 md:py-2 hover:bg-sky-600 bg-sky-700">
           Login
         </button>
+        <p className="text-sm text-center md:text-md">
+          Don&apos;t have an account yet?
+          <Link href="/register">
+            <a className="text-sky-700 hover:text-sky-400 hover:underline">
+              {' '}
+              click here{' '}
+            </a>
+          </Link>
+          to go to account list page
+        </p>
       </div>
     </div>
   );
