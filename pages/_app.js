4cb177cdcfa50b4205fb914a0308c99259ebd93e
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2022-05-30 21:06:28 +0700

    feat: install redux dan setup redux store
    install redux toolkit dan setup redux store

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,42 @@
 import '../styles/globals.css';
 import Link from 'next/link';
+import { Provider } from 'react-redux';
+import { store } from '../src/app/store';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="min-h-screen px-10 font-sans md:px-14 lg:px-24 xl:px-28 text-slate-300 w-100 bg-slate-900">
-      <header className="flex items-center justify-between px-5 py-5 mb-5 border-b-2 border-slate-800">
-        <h1 className="text-xl font-semibold md:text-3xl text-sky-500 ">
-          <Link href="/">
-            <a className="cursor-pointer hover:underline" href="">
-              Todolist App
+    <Provider store={store}>
+      <div className="min-h-screen px-10 font-sans md:px-14 lg:px-24 xl:px-28 text-slate-300 w-100 bg-slate-900">
+        <header className="flex items-center justify-between px-5 py-5 mb-5 border-b-2 border-slate-800">
+          <h1 className="text-xl font-semibold md:text-3xl text-sky-500 ">
+            <Link href="/">
+              <a className="cursor-pointer hover:underline" href="">
+                Todolist App
+              </a>
+            </Link>
+          </h1>
+          <h1 className="font-medium md:text-lg text-md text-cyan-700 hover:text-cyan-400">
+            <Link href="/logout">
+              <a className="cursor-pointer hover:underline" href="">
+                Logout
+              </a>
+            </Link>
+          </h1>
+        </header>
+        <Component {...pageProps} />
+        <footer className="flex items-center justify-center py-5 mt-10 border-t-2 border-slate-800">
+          <p>
+            Made with ❤ By{' '}
+            <a
+              className="underline text-sky-700 hover:text-sky-400"
+              href="https://elbi.vercel.app"
+            >
+              Rhafael Bijaksana
             </a>
-          </Link>
-        </h1>
-        <h1 className="font-medium md:text-lg text-md text-cyan-700 hover:text-cyan-400">
-          <Link href="/logout">
-            <a className="cursor-pointer hover:underline" href="">
-              Logout
-            </a>
-          </Link>
-        </h1>
-      </header>
-      <Component {...pageProps} />
-      <footer className="flex items-center justify-center py-5 mt-10 border-t-2 border-slate-800">
-        <p>
-          Made with ❤ By{' '}
-          <a
-            className="underline text-sky-700 hover:text-sky-400"
-            href="https://elbi.vercel.app"
-          >
-            Rhafael Bijaksana
-          </a>
-        </p>
-      </footer>
-    </div>
+          </p>
+        </footer>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/features/user/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  token: null,
+  email: null,
+  name: null,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    login: (state, action) => {
+      state.token = action.payload.token;
+      state.email = action.payload.email;
+      state.name = action.payload.name;
+    },
+    logout: (state) => {
+      state.token = null;
+      state.email = null;
+      state.name = null;
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+export default userSlice.reducer;
