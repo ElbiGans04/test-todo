00e03329e6aca1ddc482cd1c453d92857bf520cc
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2022-05-31 22:09:13 +0700

    feat: install dan setup nextjs-progressbar
    setup nextjs-progressbar agar dalam web terdapat progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,11 @@ import '../styles/globals.css';
 import Link from 'next/link';
 import { Provider } from 'react-redux';
 import { store } from '../src/app/store';
-
+import NP from 'nextjs-progressbar';
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <NP />
       <div className="min-h-screen px-10 font-sans md:px-14 lg:px-24 xl:px-28 text-slate-300 w-100 bg-slate-900">
         <header className="flex items-center justify-between px-5 py-5 mb-5 border-b-2 border-slate-800">
           <h1 className="text-xl font-semibold md:text-3xl text-sky-500 ">
