744c24c6219b16f5efcbec0c41b298dd5c8cfa15
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2022-06-01 11:28:46 +0700

    refactor: membuat component Header pada pages/_app
    membuat component Header pada pages/_app dan membuat agar dapat logout

--- pages/_app.js
+++ pages/_app.js
@@ -3,27 +3,15 @@ import Link from 'next/link';
 import { Provider } from 'react-redux';
 import { store } from '../src/app/store';
 import NP from 'nextjs-progressbar';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, userSelector } from '../src/features/user/userSlice';
+
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <NP options={{ showSpinner: false }} />
       <div className="min-h-screen px-10 font-sans md:px-14 lg:px-24 xl:px-28 text-slate-300 w-100 bg-slate-900">
-        <header className="flex items-center justify-between px-5 py-5 mb-5 border-b-2 border-slate-800">
-          <h1 className="text-xl font-semibold md:text-3xl text-sky-500 ">
-            <Link href="/">
-              <a className="cursor-pointer hover:underline" href="">
-                Todolist App
-              </a>
-            </Link>
-          </h1>
-          <h1 className="font-medium md:text-lg text-md text-cyan-700 hover:text-cyan-400">
-            <Link href="/logout">
-              <a className="cursor-pointer hover:underline" href="">
-                Logout
-              </a>
-            </Link>
-          </h1>
-        </header>
+        <Header />
         <Component {...pageProps} />
         <footer className="flex items-center justify-center py-5 mt-10 border-t-2 border-slate-800">
           <p>
@@ -42,3 +30,31 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+function Header() {
+  const dispatch = useDispatch();
+  const user = useSelector(userSelector);
+
+  return (
+    <header className="flex items-center justify-between px-5 py-5 mb-5 border-b-2 border-slate-800">
+      <h1 className="text-xl font-semibold md:text-3xl text-sky-500 ">
+        <Link href="/">
+          <a className="cursor-pointer hover:underline" href="">
+            Todolist App
+          </a>
+        </Link>
+      </h1>
+      {user.token && user.name && user.email && (
+        <h1 className="font-medium md:text-lg text-md text-cyan-700 hover:text-cyan-400">
+          <a
+            onClick={() => dispatch(logout())}
+            className="cursor-pointer hover:underline"
+            href=""
+          >
+            Logout
+          </a>
+        </h1>
+      )}
+    </header>
+  );
+}
