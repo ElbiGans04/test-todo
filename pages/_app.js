839a495e93220c57405b3aacb6f64b86049f948f
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2022-06-01 11:21:17 +0700

    refactor: mengubah option untuk nextjs-progressbar
    mengubah option untuk nextjs-progressbar pada pages/_app

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import NP from 'nextjs-progressbar';
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <NP />
+      <NP options={{ showSpinner: false }} />
       <div className="min-h-screen px-10 font-sans md:px-14 lg:px-24 xl:px-28 text-slate-300 w-100 bg-slate-900">
         <header className="flex items-center justify-between px-5 py-5 mb-5 border-b-2 border-slate-800">
           <h1 className="text-xl font-semibold md:text-3xl text-sky-500 ">
