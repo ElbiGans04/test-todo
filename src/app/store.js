9a1a9f7fe07310a156e6dd5abe1455e5444a3441
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2022-06-02 15:37:47 +0700

    feat: menambahkan state.filter pada redux
    menambahkan state.filter pada redux dan juga membuat
    src/features/filter/filterSlice.js sebagai reducernya

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../features/user/userSlice';
 import statusSlice from '../features/status/statusSlice';
+import filterSlice from '../features/filter/filterSlice';
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     status: statusSlice,
+    filter: filterSlice,
   },
 });
